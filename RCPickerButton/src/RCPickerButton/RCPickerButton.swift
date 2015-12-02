c3b9df811171351b4e87b63e1b3a902b0b8476cd
Babkin Nick <babkin.nick@gmail.com>
2015-12-02 15:30:11 +0200

    Initial commit

---
+++ RCPickerButton/src/RCPickerButton/RCPickerButton.swift
@@ -0,0 +1,9 @@
+//
+//  RCPickerButton.swift
+//  RCPickerButton
+//
+//  Created by Nick on 2/12/15.
+//  Copyright © 2015 spromicky. All rights reserved.
+//
+
+import Foundation
---
+++ RCPickerButton/src/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RCPickerButton
+//
+//  Created by Nick on 2/12/15.
+//  Copyright © 2015 spromicky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
